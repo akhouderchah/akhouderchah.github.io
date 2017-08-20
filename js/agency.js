20b3d889322fa005d55f56169df7c26ccede93c5
Alex Khouderchah <alex.khouderchah@gmail.com>
2017-08-20 12:57:55 -0700

    Added Articles page

--- js/agency.js
+++ js/agency.js
@@ -5,6 +5,7 @@
 
     $(document).ready(function() {
 		var prevScroll = 0;
+		var listSpace = $('.list-spacer').css('margin-bottom');
         $(window).scroll(function() {
             if ($(this).scrollTop() > 500) {
 				if(prevScroll <= 500) {
@@ -15,6 +16,28 @@
             }
 			prevScroll = $(this).scrollTop();
         });
+
+		$('.list-hider').click(function(){
+			var hideeElem = $(this).closest('.timeline-heading').find('.list-hidee');
+			var spaceElem = $(this).closest('.timeline-heading').find('.list-spacer');
+			if( hideeElem.css('display') == 'none' ) {
+				hideeElem.last().fadeIn(300, 'linear');
+				hideeElem.css('display', 'block');
+
+				spaceElem.css('marginBottom', '-10px')
+				spaceElem.animate({ marginBottom: '0px' }, 300);
+			} else {
+				hideeElem.last().fadeOut(300, function(){
+					spaceElem.css('marginBottom', listSpace);
+				} );
+				/*
+				setTimeout(function(){
+					spaceElem.animate({ marginBottom: '100px' }, 50);
+				}, 250);
+*/
+			}
+			//$(this).closest('.timeline-heading').find('.list-hidee').last().fadeOut(300, 'linear');
+		});
     });
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
